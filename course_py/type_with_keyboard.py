1e07df550c77afae2fb0eed34f3cec1fdf8d958d
wuyak <136773195+wuyak@users.noreply.github.com>
2024-11-07 23:33:21 +0800

    finished hw06

---
+++ course_py/type_with_keyboard.py
@@ -0,0 +1,75 @@
+
+LOWERCASE_LETTERS = 'abcdefghijklmnopqrstuvwxyz'
+
+class CapsLock:
+    def __init__(self):
+        self.pressed = 0
+
+    def press(self):
+        self.pressed += 1
+
+class Button:
+    """A button on a keyboard.
+
+    >>> f = lambda c: print(c, end='')  # The end='' argument avoids going to a new line
+    >>> k, e, y = Button('k', f), Button('e', f), Button('y', f)
+    >>> s = e.press().press().press()
+    eee
+    >>> caps = Button.caps_lock
+    >>> t = [x.press() for x in [k, e, y, caps, e, e, k, caps, e, y, e, caps, y, e, e]]
+    keyEEKeyeYEE
+    >>> u = Button('a', print).press().press().press()
+    A
+    A
+    A
+    """
+    caps_lock = CapsLock()
+
+    def __init__(self, letter, output):
+        assert letter in LOWERCASE_LETTERS
+        self.letter = letter
+        self.output = output
+        self.pressed = 0
+
+    def press(self):
+        """Call output on letter (maybe uppercased), then return the button that was pressed."""
+        self.pressed += 1
+        "*** YOUR CODE HERE ***"
+        if self.caps_lock.pressed % 2 == 0:
+            self.output(self.letter)
+        else:
+            self.output(self.letter.upper())
+        return self
+
+class Keyboard:
+    """A keyboard.
+
+    >>> Button.caps_lock.pressed = 0  # Reset the caps_lock key
+    >>> bored = Keyboard()
+    >>> bored.type('hello')
+    >>> bored.typed
+    ['h', 'e', 'l', 'l', 'o']
+    >>> bored.keys['l'].pressed
+    2
+
+    >>> Button.caps_lock.press()
+    >>> bored.type('hello')
+    >>> bored.typed
+    ['h', 'e', 'l', 'l', 'o', 'H', 'E', 'L', 'L', 'O']
+    >>> bored.keys['l'].pressed
+    4
+    """
+    """
+    the Button(c, self.typed.append) would call when pressed,
+    then self.output is self.typed.append(c.letter)
+    """
+    def __init__(self):
+        self.typed = []
+        self.keys =  {c: Button(c, self.typed.append) for c in LOWERCASE_LETTERS}# Try a dictionary comprehension!
+
+    def type(self, word):
+        """Press the button for each letter in word."""
+        assert all([w in LOWERCASE_LETTERS for w in word]), 'word must be all lowercase'
+        "*** YOUR CODE HERE ***"
+        for w in word:
+            self.keys[w].press()
